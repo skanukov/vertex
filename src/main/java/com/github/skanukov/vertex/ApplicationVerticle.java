e65f167a02cfad830cef582cb3e0136f1e3a7fe3
Sergey Kanukov <s.kanukov@gmail.com>
2016-08-17 20:30:08 +0300

    Use Logback as default logger

--- src/main/java/com/github/skanukov/vertex/ApplicationVerticle.java
+++ src/main/java/com/github/skanukov/vertex/ApplicationVerticle.java
@@ -2,17 +2,23 @@ package com.github.skanukov.vertex;
 
 import io.vertx.core.AbstractVerticle;
 import io.vertx.core.http.HttpServer;
+import io.vertx.core.logging.Logger;
+import io.vertx.core.logging.LoggerFactory;
 import io.vertx.ext.web.Router;
 
 /**
  * Creates, configs and starts the HTTP server.
  */
 public final class ApplicationVerticle extends AbstractVerticle {
+    private static final Logger logger = LoggerFactory.getLogger(ApplicationVerticle.class);
+
     /**
      * Creates and starts the server, sets the router dispatcher.
      */
     @Override
     public void start() {
+        logger.info("Verticle {} created", this);
+
         HttpServer server = vertx.createHttpServer();
         Router router = Router.router(vertx);
         router.route("/").handler(rc ->
